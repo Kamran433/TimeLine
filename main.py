1dfc041223a2eb2d12315a1c1b1badb4fe8b868b
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:32:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# age=input("what is your age?\n")
+# left=90-int(age)
+# days=int(left)*365
+# months=int(left)*12
+# weeks=int(left)*52
+# print(f"you have {days} days,{months} months and {weeks} weeks left.")
